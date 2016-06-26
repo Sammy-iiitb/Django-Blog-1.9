69d4006934b244fcfe0d20c9dd085fe9bfd4368c
sammme <samyak.upadhyay123@gmail.com>
2016-06-26 21:30:44 +0530

    customise admin of posts

--- posts/admin.py
+++ posts/admin.py
@@ -4,4 +4,13 @@ from django.contrib import admin
 
 from .models import Post
 
-admin.site.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ("_unicode_", "updated", "timestamp")
+    list_display_links = ["updated"]
+    list_filter = ["updated", "timestamp"]
+    search_fields = ["title", "content"]
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostModelAdmin)
